317e73870353648a7cefdcc2c601062ddbe063ba
dipanshumilmile <150071604+dipanshumilmile@users.noreply.github.com>
2025-12-15 22:53:58 +0530

    Time: 2 ms (61.78%) | Memory: 44.7 MB (39.97%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,37 +1,23 @@
 class Solution {
     List<String> res = new ArrayList<>();
-    private boolean isValid(String s){
-        int count=0;
-        for(char ch : s.toCharArray()){
-            if(ch=='('){
-                count++;
-            }
-            else{
-                count--;
-            }
-            if(count<0){
-                return false;
-            }
 
-        }
-        if(count==0) return true;
-        return false;
-    }
-    private void solve(String curr, int n){
-         if(curr.length()==2*n){
-            if(isValid(curr)){
-                res.add(curr);
-            }
+    private void backtrack(String curr, int open, int close, int n) {
+        if (curr.length() == 2 * n) {
+            res.add(curr);
             return;
-         }
-         solve(curr + "(", n);
-        solve(curr + ")", n);
+        }
 
-    }
-    public List<String> generateParenthesis(int n) {
-       
-            String curr="";
-             solve(curr, n);
-             return res;
+        if (open < n) {
+            backtrack(curr + "(", open + 1, close, n);
+        }
+
+        if (close < open) {
+            backtrack(curr + ")", open, close + 1, n);
         }
     }
+
+    public List<String> generateParenthesis(int n) {
+        backtrack("", 0, 0, n);
+        return res;
+    }
+}
