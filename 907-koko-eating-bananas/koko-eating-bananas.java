325a74e915bea6407fb0504c3de5e6d22f0882c1
dipanshumilmile <150071604+dipanshumilmile@users.noreply.github.com>
2026-01-02 16:18:50 +0530

    Time: 22 ms (35.90%) | Memory: 48 MB (39.57%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,32 @@
+class Solution {
+    private boolean canEatAll(int[] piles, int mid, int h){
+        int totalEat=0;
+        for(int pile : piles){
+            totalEat += pile/mid;
+            if(pile%mid != 0){
+                totalEat++;
+            }
+
+        }
+        return totalEat <= h;
+
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int l = 1;
+        int r = piles[piles.length-1];
+
+
+        while(l<r){
+            int mid = l + (r-l)/2;
+            if(canEatAll(piles, mid, h)){
+                r = mid;
+            }
+            else{
+                l = mid+1;
+            }
+
+        }
+        return l;
+    }
+}
