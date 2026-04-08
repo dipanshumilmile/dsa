20c9b547cb297b5f2de5c1030e9d87bcd0665b03
dipanshumilmile <150071604+dipanshumilmile@users.noreply.github.com>
2026-04-08 17:20:35 +0530

    Time: 1 ms (98.33%) | Memory: 46.7 MB (39.28%) - LeetSync

---
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private TreeNode dfs(TreeNode node, List<TreeNode> res, Set<Integer> set){
+        if(node == null) return null;
+
+        node.left=dfs(node.left, res, set);
+        node.right=dfs(node.right,res, set);
+
+        if(set.contains(node.val)){
+            if(node.left != null) res.add(node.left);
+            if(node.right != null) res.add(node.right);
+            return null;
+        }
+        return node;
+    }
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int val : to_delete){
+            set.add(val);
+        }
+        List<TreeNode> res = new ArrayList<>();
+        root = dfs(root, res, set);
+        if(root != null){
+            res.add(root);
+        }
+        return res;
+    }
+}
