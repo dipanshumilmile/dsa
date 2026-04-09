b3444c6fac394ba4c7867a697b3b2ac3775bd046
dipanshumilmile <150071604+dipanshumilmile@users.noreply.github.com>
2026-04-10 01:31:35 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (100.00%) - LeetSync

---
+++ 623-add-one-row-to-tree/add-one-row-to-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private TreeNode add(TreeNode root, int val, int depth, int curr){
+        if(root == null) return null;
+
+        if(curr == depth-1){
+            TreeNode left = root.left;
+            TreeNode right = root.right;
+
+            root.left = new TreeNode(val);
+            root.right = new TreeNode(val);
+            root.left.left = left;
+            root.right.right = right;
+            return root;
+        }
+        
+        root.left = add(root.left, val,  depth,  curr+1);
+        root.right = add( root.right, val,  depth,  curr+1);
+        return root;
+    }
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if(depth == 1){
+            TreeNode newRoot = new TreeNode(val);
+            newRoot.left=root;
+            return newRoot;
+        }
+        int curr = 1;
+        return add( root, val,  depth,  curr);
+    }
+}
