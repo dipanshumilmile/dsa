069d95cd9575f920a24242f5987ee98b4beeaf57
dipanshumilmile <150071604+dipanshumilmile@users.noreply.github.com>
2026-09-03 14:35:49 +0530

    Time: 1 ms (100.00%) | Memory: 48.5 MB (93.83%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,28 +1,30 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int []res = new int [nums2.length + nums1.length];
-        int i=0; int j=0;
-        int k=0;
-        while( i != nums1.length && j != nums2.length){
-            if(nums1[i] < nums2[j]){
-                res[k++] = nums1[i++];
-            }else{
-                res[k++] = nums2[j++];
-            }
+        if(nums1.length > nums2.length){
+            return findMedianSortedArrays(nums2,nums1);
         }
-        while(i<nums1.length){
-            res[k++] = nums1[i++];
-        }
-        while(j<nums2.length){
-            res[k++] = nums2[j++];
-        }
-int len = res.length;
-        
-        if((res.length) % 2 ==1){
-            return res[res.length/2];
+        int m = nums1.length;
+        int n = nums2.length;
+        int l  = 0;
+        int r = m;
 
-        }else{
-             return (res[len / 2 - 1] + res[len / 2]) / 2.0;
-        }
+        while(l <= r)
+        {
+            int px = l + (r-l)/2;
+            int py = (m+n+1)/2 - px;
+
+            int x1 = px == 0 ? Integer.MIN_VALUE : nums1[px-1];
+            int x2 = py == 0 ? Integer.MIN_VALUE : nums2[py-1];
+
+            int x3 = px == m ? Integer.MAX_VALUE : nums1[px];
+            int x4 = py == n ?  Integer.MAX_VALUE : nums2[py];
+
+            if(x1 <= x4 && x2 <= x3 ){
+            if((m+n)%2 == 1) return Math.max(x1,x2);
+            else return (Math.max(x1,x2) + Math.min(x3, x4))/2.0;
+
+            }if(x1>x4) r = px-1;
+            else l = px+1;
+        } return -1;
     }
 }
