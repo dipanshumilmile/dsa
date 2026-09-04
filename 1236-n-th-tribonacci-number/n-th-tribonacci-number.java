f1a691098fd005a7486261c63e8dd92891584804
dipanshumilmile <150071604+dipanshumilmile@users.noreply.github.com>
2026-09-04 14:55:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (53.34%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    Integer []dp;
+    public int tribonacci(int n) {
+        dp = new Integer[n+1];
+        return helper(n);
+    }
+
+    private int helper(int n){
+        if(n ==0) return 0;
+        if(n==1 || n==2) return 1;
+        if(dp[n] != null) return dp[n];
+
+        return dp[n] = helper(n-1) + helper(n-2) + helper(n-3);
+    }
+}
