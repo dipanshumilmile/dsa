a4b412ff31443e62705689bf49df1da0aa35bd5d
dipanshumilmile <150071604+dipanshumilmile@users.noreply.github.com>
2026-09-04 14:42:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (54.80%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    Integer dp[];
+    public int climbStairs(int n) {
+        dp = new Integer[n+1];
+        return helper(n);
+    }
+    private int helper(int n){
+       if(n<=2) return n;
+       if(dp[n]!=null) return dp[n];
+       return dp[n] = helper(n-1) + helper(n-2);
+    }
+}
